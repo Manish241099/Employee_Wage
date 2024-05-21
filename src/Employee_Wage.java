80133a4b1e4fcb9f3fe9d83eb5bd84aca2ea7f36
Manish241099 <manishkhade241099@gmail.com>
2024-05-20 21:48:08 -0700

    UC added

--- src/Employee_Wage.java
+++ src/Employee_Wage.java
@@ -3,5 +3,13 @@ import java.sql.SQLOutput;
 public class Employee_Wage {
     public static void main(String[] args) {
         System.out.println("welcome to Employee_Wage");
+        int Full_Time = 1;
+
+        double emp_check = Math.floor(Math.random()*10 % 2);
+        if(Full_Time == emp_check)
+            System.out.println("Employee is present");
+        else
+            System.out.println("Employee is not present");
     }
-}
+    }
+
