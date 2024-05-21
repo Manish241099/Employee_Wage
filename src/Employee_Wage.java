903744733d9fb607de2a3d60b38d0746211351e7
Manish241099 <manishkhade241099@gmail.com>
2024-05-21 00:22:50 -0700

    UC3_using_switch_case

--- src/Employee_Wage.java
+++ src/Employee_Wage.java
@@ -1,11 +1,9 @@
 import java.sql.SQLOutput;
 
 public class Employee_Wage {
-
-    static int FULL_TIME_EMP = 1;
-    static int PART_TIME_EMP=2;
+    
    static   int WAGE_PER_HOUR= 20;
-    static int FULL_DAY_HOUR= 2;
+
     static int FULL_TIME_HOUR = 8;
    static int PART_TIME_HOUR=4;
 
@@ -14,31 +12,29 @@ public class Employee_Wage {
     static double DAILY_EMP_WAGE=0;
 
     public static void main(String[] args) {
-          int emp_hour=0;
-        int daily_emp_wage=1;
+        int empcheck = (int)Math.floor(Math.random()*10%3);
 
-        double empcheck = Math.floor(Math.random()*10%3);
+        switch (empcheck) {
+            case 1:
+                DAILY_EMP_WAGE = FULL_TIME_HOUR * WAGE_PER_HOUR;
+                System.out.println("Employee are present for full time.");
+                System.out.println("Employee Daily Wage is : " + DAILY_EMP_WAGE);
+                break;
 
-        if(empcheck == FULL_TIME_EMP){
-            System.out.println("Employee are present for full time.");
-            daily_emp_wage = FULL_TIME_HOUR * WAGE_PER_HOUR;
+            case 2:
+                DAILY_EMP_WAGE = PART_TIME_HOUR * WAGE_PER_HOUR;
+                System.out.println("Employee are present for part time.");
+                System.out.println("Employee wage is : "+DAILY_EMP_WAGE);
+                break;
+
+            default:
+                System.out.println("Employee are not present");
+                DAILY_EMP_WAGE=0;
+                System.out.println("Employee Daily Wage is : " + DAILY_EMP_WAGE);
+                break;
+
+
         }
-        else if (empcheck == PART_TIME_EMP) {
-            System.out.println("Employee are present for part time.");
-            daily_emp_wage = PART_TIME_HOUR * WAGE_PER_HOUR;
-        }
-        else{
-            System.out.println("Employee are not present");
-            daily_emp_wage=0;
-        }
-
-        System.out.println("Employee Daily Wage is : " + daily_emp_wage);
-
-
-
-
-        System.out.println("Employee Daily Wage is : " + DAILY_EMP_WAGE);
-
     }
     }
 
