ab5f27d6f35d78667bdc502daaa6fcf9956b2baf
Manish241099 <manishkhade241099@gmail.com>
2024-05-21 02:45:45 -0700

    UC6

--- src/Employee_Wage.java
+++ src/Employee_Wage.java
@@ -18,9 +18,15 @@ public class Employee_Wage {
 
     public static void main(String[] args) {
         int total_wage =0;
+        int total_emp_work_hour=0;
+        int EMP_WORK_HOUR=0;
+        int total_emp_work_day=0;
 
-            for (int i = 0; i < EMP_WORK_DAYS; i++) {
-                int empcheck = (int) Math.floor(Math.random() * 10 % 3);
+
+
+        while(total_emp_work_hour<= EMP_WORK_HOUR && total_emp_work_day < EMP_WORK_DAYS ){
+            total_emp_work_day++;
+            int empcheck = (int) Math.floor(Math.random() * 10 % 3);
 
                 if (empcheck == FULL_TIME_EMP) {
                     System.out.println("Employee are present for full time.");
