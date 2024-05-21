eb5a3147b964bbc153c7c9c2991e60efa3537653
Manish241099 <manishkhade241099@gmail.com>
2024-05-21 02:20:39 -0700

    UC5_using_forLoop

--- src/Employee_Wage.java
+++ src/Employee_Wage.java
@@ -1,40 +1,42 @@
 import java.sql.SQLOutput;
+import java.sql.Wrapper;
 
 public class Employee_Wage {
-    
+
    static   int WAGE_PER_HOUR= 20;
 
     static int FULL_TIME_HOUR = 8;
    static int PART_TIME_HOUR=4;
+   static int EMP_WORK_DAYS=2;
+   static int FULL_TIME_EMP=1;
+    static int PART_TIME_EMP=2;
+
 
 
 
     static double DAILY_EMP_WAGE=0;
 
     public static void main(String[] args) {
-        int empcheck = (int)Math.floor(Math.random()*10%3);
+        int total_wage =0;
 
-        switch (empcheck) {
-            case 1:
-                DAILY_EMP_WAGE = FULL_TIME_HOUR * WAGE_PER_HOUR;
-                System.out.println("Employee are present for full time.");
+            for (int i = 0; i < EMP_WORK_DAYS; i++) {
+                int empcheck = (int) Math.floor(Math.random() * 10 % 3);
+
+                if (empcheck == FULL_TIME_EMP) {
+                    System.out.println("Employee are present for full time.");
+                    DAILY_EMP_WAGE = FULL_TIME_HOUR * WAGE_PER_HOUR;
+                } else if (empcheck == PART_TIME_EMP) {
+                    System.out.println("Employee are present for part time.");
+                    DAILY_EMP_WAGE = PART_TIME_HOUR * WAGE_PER_HOUR;
+                } else {
+                    System.out.println("Employee are not present");
+                    DAILY_EMP_WAGE = 0;
+                }
+                total_wage += DAILY_EMP_WAGE;
                 System.out.println("Employee Daily Wage is : " + DAILY_EMP_WAGE);
-                break;
-
-            case 2:
-                DAILY_EMP_WAGE = PART_TIME_HOUR * WAGE_PER_HOUR;
-                System.out.println("Employee are present for part time.");
-                System.out.println("Employee wage is : "+DAILY_EMP_WAGE);
-                break;
-
-            default:
-                System.out.println("Employee are not present");
-                DAILY_EMP_WAGE=0;
-                System.out.println("Employee Daily Wage is : " + DAILY_EMP_WAGE);
-                break;
-
-
+            }
+            System.out.println("The Wages for a month is :" + total_wage);
         }
     }
-    }
+
 
