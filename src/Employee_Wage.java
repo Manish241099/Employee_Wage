a3852275a88fab8909cb1e6fcb20e79277309205
Manish241099 <manishkhade241099@gmail.com>
2024-05-20 22:27:49 -0700

    UC2_EMP_WAGE_CALC_DAILY_WAGE

--- src/Employee_Wage.java
+++ src/Employee_Wage.java
@@ -1,15 +1,27 @@
 import java.sql.SQLOutput;
 
 public class Employee_Wage {
+    static int WAGE_PER_HOUR= 20;
+    static int FULL_DAY_HOUR= 8;
+   static double DAILY_EMP_WAGE=0;
+
     public static void main(String[] args) {
         System.out.println("welcome to Employee_Wage");
-        int Full_Time = 1;
+        int FULL_TIME= 1;
 
         double emp_check = Math.floor(Math.random()*10 % 2);
-        if(Full_Time == emp_check)
+        if(FULL_TIME == emp_check)
             System.out.println("Employee is present");
         else
             System.out.println("Employee is not present");
+        double empcheck = Math.floor(Math.random()*10%2);
+
+        if(empcheck == FULL_TIME){
+            DAILY_EMP_WAGE = WAGE_PER_HOUR* FULL_DAY_HOUR;
+        }
+
+        System.out.println("Employee Daily Wage is : " + DAILY_EMP_WAGE);
+
     }
     }
 
